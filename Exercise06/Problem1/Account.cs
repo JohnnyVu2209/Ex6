33d55b3c7d549d1856c77c18fd50fef873953870
thanglong503 <tranthanglong503@gmail.com>
2019-10-17 14:48:47 +0700

    bank2

--- Exercise06/Problem1/Account.cs
+++ Exercise06/Problem1/Account.cs
@@ -11,22 +11,33 @@ namespace Problem1
         int money;
         public Account(string id,int amount)
         {
+            Console.WriteLine("Users: {0}",id);
             money = amount;
+            Console.WriteLine("Balance: {0}",money);
            
           
         }
         public void Withdraw()
         {
             Console.WriteLine("Please insert money you want to Withdraw");
-            int amount = int.Parse(Console.ReadLine());  
-            money -= amount;
-            
+            int amount = int.Parse(Console.ReadLine());
+            if(amount > Balance())
+            {
+                Console.WriteLine("out of money");
+            }
+            else
+            {
+                money -= amount;
+                Console.WriteLine("Withdraw successful");
+            }
+                        
         }
         public void Deposit()
         {
             Console.WriteLine("Please insert money you want to Deposit");
             int amount = int.Parse(Console.ReadLine());
             money += amount;
+            Console.WriteLine("Deposit successful");
         }
         public int Balance()
         {
--- Exercise06/Problem1/Program.cs
+++ Exercise06/Problem1/Program.cs
@@ -13,67 +13,24 @@ namespace Problem1
         {
             Console.WriteLine("Insert your ATM card: ");
             string cardID = Console.ReadLine();
-            string FilePath = @"D:\Vu\C#\Account.txt";
-
-
-            string ID = "";string Pin = "";
-            FileStream fs = new FileStream(FilePath, FileMode.Open);
-            StreamReader str = new StreamReader(fs, Encoding.UTF8);
-            String[] ListAccount = str.ReadToEnd().Split(' ');
-            for (int i = 0; i < ListAccount.Length; i++)
-            {
-                String[] AtmAccount = ListAccount[i].Split('_');
-                for (int j = 0; j < AtmAccount.Length; j++)
-                {
-                    ID = AtmAccount[0];
-                    Pin = AtmAccount[1];
-                    Console.WriteLine(ID);
-                }
-            }
+            Account acc = new Account(cardID, 200000);
+            Console.WriteLine("What you want to do? Deposit(D) or Withdraw(W)");
+            string choose = Console.ReadLine();
+            if (choose == "D" || choose == "d")
+                acc.Deposit();
+            else if (choose == "W" || choose == "w")
+                acc.Withdraw();
             
-            str.Close();
-            if(cardID == ID)
-            {
-                Console.Write("Pin: ");
-                string pass = Console.ReadLine();
-                if(pass == Pin)
-                {
-                    Account acc = new Account(cardID,200000);
-                    Console.WriteLine("ID Acount: {0}", cardID);
-                    Console.WriteLine("Balance: {0}", acc.Balance());
-                    Console.WriteLine();
-                   
-                    while(true)
-                    {
-                        Console.WriteLine("You want: Withdraw(W) Or Deposit(D) or End(E)");
-                        string want = Console.ReadLine();
-                        if (want == "W" || want == "w")
-                        { acc.Withdraw(); Console.WriteLine("balance: {0}", acc.Balance()); }
+                
 
-                        else if (want == "D" || want == "d")
-                        { acc.Deposit(); Console.WriteLine("balance: {0}", acc.Balance()); }
-                        else if (want == "e" || want == "E")
-                            break;
-                    }
-                    
-                }
-            }
-            else
-            {
-                Console.WriteLine("Wrong ID");
-            }
-        
         }
-
-        
-        static void ReadAccount(string FilePath, out string ID , out string Pin)
-        {
-            ID = ""; Pin = "";
-            
-            
-            
-            
-        }
-    }
-
+    }        
 }
+
+        
+        
+            
+            
+            
+            
+        
