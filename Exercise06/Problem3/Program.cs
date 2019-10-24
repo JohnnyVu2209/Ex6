4f598d4e3f49d766818961fff321eb5efe536b89
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-24 13:58:05 +0700

    Problem3

---
+++ Exercise06/Problem3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Q = new Queue();
+            Q.Enqueue(1);
+            Q.Enqueue(2);
+            Q.Enqueue(3);
+
+            //Console.WriteLine(Q.Dequeue());
+            Console.WriteLine(Q.Dequeue());
+            Console.WriteLine(Q.Count());
+        }
+    }
+}
---
+++ Exercise06/Problem3/Queue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3
+{
+    class Queue
+    {
+        int[] s = new int[100];
+        int i = 0;
+        int sta = 0;
+        public void Enqueue(int item)
+        {
+            s[i] = item;
+            i++;
+            sta++;
+        }
+        public int Dequeue()
+        {
+            int queue = s[sta - i];
+            i--;
+            return queue;
+        }
+        public int Count()
+        {
+            return i;
+        }
+        public int Peek()
+        {
+            return s[sta - i];
+        }
+    }
+}
