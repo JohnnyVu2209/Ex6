4f975bd3d55adfb66c2c56ff28b04fbec1053be3
JohnnyVu2209 <johnnyv2209@gmail.com>
2019-10-24 13:55:32 +0700

    Problem2

---
+++ Exercise06/Problem2/MyStack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+   
+    public class MyStack
+    {
+         int[] s = new int[100];
+         int i = 0;
+        public void Put(int item)
+         {
+             s[i] = item;
+             i++;
+         }
+        public int Pop()
+        {
+            i--;
+            return (s[i]);
+        }
+        public int Count()
+        {
+            return i;
+        }
+        public int Peek()
+        {
+            return s[i - 1];
+            //hello
+        }
+
+    }
+    
+}
---
+++ Exercise06/Problem2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack s = new MyStack();
+            s.Put(1);
+            s.Put(2);
+            s.Put(4);
+
+            //Console.WriteLine(s.Pop());
+            //Console.WriteLine(s.Pop());
+            Console.WriteLine(s.Peek());
+            
+        }
+    }
+}
